c72e5bc8416edc51f2811505694817c0d12b4f12
Viktors Jenovs <jviktors@gmail.com>
2017-02-28 13:19:57 +0200

    Fix ip address parsing

--- header-parser/router.js
+++ header-parser/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  const ipaddress = req.headers['host'];
+  const ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const language = req.headers['accept-language'];
   const software = req.headers['user-agent'];
 
