801c316b7cabed7854234a8054366e6d94910e32
Viktors Jenovs <jviktors@gmail.com>
2017-03-09 10:26:14 +0200

    Add weather request logic

---
+++ src/weather/api.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+const { WEATHER_API_KEY } = process.env;
+
+
+function getWeather(lat, lon, lang) {
+  console.log(lat, lon);
+  let weatherUrl = `https://api.wunderground.com/api/${WEATHER_API_KEY}/conditions/q/${lat},${lon}.json`;
+  let forecastUrl = `https://api.wunderground.com/api/${WEATHER_API_KEY}/forecast/q/${lat},${lon}.json`;
+  return Promise.all([
+    fetch(weatherUrl).then(res => res.json()),
+    fetch(forecastUrl).then(res => res.json())
+  ])
+  .catch(e => console.log(e));
+}
+
+module.exports = {
+  getWeather
+}
--- src/weather/router.js
+++ src/weather/router.js
@@ -1,14 +1,25 @@
 const router = require('express').Router();
-const fetch = require('node-fetch');
-const { WEATHER_API_KEY } = process.env;
+const bodyParser = require('body-parser');
+
+const { getWeather } = require('./api');
 
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-router.get('/:lat/:lon', (req, res) => {
-  res.send([req.params.lat, req.params.lon]);
+router.use(bodyParser.json());
+
+router.post('/', (req, res) => {
+  const { lat, lon, lang } = req.body;
+
+  getWeather(lat, lon, lang)
+  .then(([weather, forecast]) => {
+    res.send({
+      weather: weather.current_observation,
+      forecast: forecast.forecast})
+  })
+  .catch(e => console.log(e));
 });
 
 module.exports = router;
