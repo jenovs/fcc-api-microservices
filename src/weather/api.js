facadb14e7994d56a22ffc64391c1057163be6c4
Viktors Jenovs <jviktors@gmail.com>
2017-03-09 10:31:39 +0200

    Add language support to API

--- src/weather/api.js
+++ src/weather/api.js
@@ -4,9 +4,8 @@ const { WEATHER_API_KEY } = process.env;
 
 
 function getWeather(lat, lon, lang) {
-  console.log(lat, lon);
-  let weatherUrl = `https://api.wunderground.com/api/${WEATHER_API_KEY}/conditions/q/${lat},${lon}.json`;
-  let forecastUrl = `https://api.wunderground.com/api/${WEATHER_API_KEY}/forecast/q/${lat},${lon}.json`;
+  let weatherUrl = `https://api.wunderground.com/api/${WEATHER_API_KEY}/conditions/lang:${lang}/q/${lat},${lon}.json`;
+  let forecastUrl = `https://api.wunderground.com/api/${WEATHER_API_KEY}/forecast/lang:${lang}/q/${lat},${lon}.json`;
   return Promise.all([
     fetch(weatherUrl).then(res => res.json()),
     fetch(forecastUrl).then(res => res.json())
